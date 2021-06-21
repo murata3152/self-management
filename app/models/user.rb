f822614eaa5ea44cd070483acb0bb2f3fb376875
murata3152 <kntnkmr6@gmail.com>
2021-06-22 01:36:19 +0900

    index(仮)作成

---
+++ app/models/user.rb
@@ -0,0 +1,24 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :post
+  has_many :buys
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Please set including both letters and numbers' 
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    with_options format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."} do
+      validates :last_name
+      validates :first_name
+    end
+    with_options format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."} do
+      validates :last_name_details
+      validates :first_name_details
+    end
+  end
+end
