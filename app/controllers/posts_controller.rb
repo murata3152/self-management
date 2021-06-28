0d7d28b93f301e3461fe573c52fc239d30dcc951
murata3152 <kntnkmr6@gmail.com>
2021-06-29 00:56:56 +0900

    Basic認証実装完了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,18 @@ class PostsController < ApplicationController
 
   def index
   end
+
+  def new
+    @pot = Post.new 
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      rennder :new
+    end
+    
+  end
 end
